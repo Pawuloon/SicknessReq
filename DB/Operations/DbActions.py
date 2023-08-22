c1ec3476bcc1de774f5fb905ee21a549c12c10cd
Pawuloon <pdembicki1@gmail.com>
2023-08-22 14:32:40 +0200

    up

--- DB/Operations/DbActions.py
+++ DB/Operations/DbActions.py
@@ -27,3 +27,29 @@ class DbActions:
         self.cursor.execute("UPDATE users SET permission = ? WHERE name = ?", (permission, name))
         self.conn.commit()
         self.conn.close()
+
+    # Sicknesses part of db ops
+
+    # Add sickness to database
+    def addSickness(self, name, description, symptoms, treatment):
+        self.cursor.execute("INSERT INTO Sicknesses (name, description, symptoms, treatment) VALUES (?, ?, ?, ?)",
+                            (name, description, symptoms, treatment))
+        self.conn.commit()
+        self.conn.close()
+
+    # Get sicknesses from database
+    def getSicknesses(self):
+        self.cursor.execute("SELECT name, description, symptoms, treatment FROM Sicknesses")
+        return self.cursor.fetchall()
+
+    # Get sickness from database
+    def getSickness(self, name):
+        self.cursor.execute("SELECT name FROM Sicknesses WHERE name = ?", (name,))
+        return self.cursor.fetchone()
+
+    # Update sickness in database
+    def updateSickness(self, name, description, symptoms, treatment):
+        self.cursor.execute("UPDATE Sicknesses SET description = ?, symptoms = ?, treatment = ? WHERE name = ?",
+                            (description, symptoms, treatment, name))
+        self.conn.commit()
+        self.conn.close()
